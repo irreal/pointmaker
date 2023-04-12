273983d1d7454f259fba6b21537d264767bffae3
Milos Spasojevic <milos.s.pfc@gmail.com>
2023-04-12 09:36:22 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="flex justify-center items-center h-full">
+	<h1 class="text-6xl md:text-9xl-text-9xl text-center">Da li baš mora?</h1>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{svelte,htm,html}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
